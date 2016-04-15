10cfe4a8cbf4ea878438f032c333f33b6205e734
Satish Jaiswal <ictish@gmail.com>
2016-04-16 03:12:21 +0900

    login

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "satish";
+$db = "artcart_aimday";
+
+$host = "http://www.artcart.local";
+$app_name = "aimDay";
+$app_folder = "app";
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require_once("config/constants.php");?>
+
+<?php
+header("Location:".$host."/".$app_name."/".$app_folder."/login.php");
+?>
