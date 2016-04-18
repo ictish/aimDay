1e2961db0f1aad4d0c6d7ca9e6cbcb459ce26dff
Satish Jaiswal <ictish@gmail.com>
2016-04-18 14:39:18 +0900

    MVC cleaner

--- app/logout.php
+++
@@ -1,8 +0,0 @@
-<?php require_once(dirname(__DIR__)."/config/constants.php");?>
-
-<?php
-session_start();
-session_unset();
-session_destroy();
-header("Location:".$host."/".$app_name."/".$app_folder."/login.php?msg=logged_out");
-?>
